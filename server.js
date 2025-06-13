927f342149f8bfa365af4e6a5362f7e17dd6ab79
Junior Vach <juniordesouzavach016@gmail.com>
2025-06-13 16:36:44 -0300

    Add files via upload
    Primeira parte do projeto

---
+++ server.js
@@ -0,0 +1,38 @@
+import express, { request, response } from 'express'
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+
+
+const prisma = new PrismaClient()
+
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res) => {
+  const allUsers = await prisma.user.findMany()
+  res.status(200).json(allUsers)
+})
+
+
+app.listen(3000)
+
+
+
+//JuniorVach
+//g81U8s7sTslnFwvV
